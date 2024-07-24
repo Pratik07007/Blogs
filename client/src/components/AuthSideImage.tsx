f8db25adbc7b70b46782cd18190459b7a1250394
Pratik07007 <s.dhimal006@gmail.com>
2024-07-25 00:45:03 +0545

    updated the favicon

--- client/src/components/AuthSideImage.tsx
+++ client/src/components/AuthSideImage.tsx
@@ -1,7 +1,7 @@
 const AuthSideImage = () => {
   return (
     <>
-      <div className="md:w-1/2 w-screen h-96 bg-green-200 md:h-[90vh]">
+      <div className="md:w-1/2 w-screen h-96 bg-black md:h-[90vh]">
         <img className="h-full w-full" src="https://res.cloudinary.com/delnndqb2/image/upload/v1721847336/ngolxv7gr1qmcg2tdpae.jpg" alt="" />
       </div>
     </>
