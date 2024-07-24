2541512163e018e5d1afb37a570c93bc815a3c0d
Pratik07007 <s.dhimal006@gmail.com>
2024-07-25 00:41:50 +0545

    updated the right image

--- client/src/components/AuthSideImage.tsx
+++ client/src/components/AuthSideImage.tsx
@@ -2,7 +2,7 @@ const AuthSideImage = () => {
   return (
     <>
       <div className="md:w-1/2 w-screen h-96 bg-green-200 md:h-[90vh]">
-        <img className="h-full w-full" src="https://res.cloudinary.com/delnndqb2/image/upload/v1721820549/blogs_images/muntn0voq2gtixzy4xyb.jpg" alt="" />
+        <img className="h-full w-full" src="https://res.cloudinary.com/delnndqb2/image/upload/v1721847336/ngolxv7gr1qmcg2tdpae.jpg" alt="" />
       </div>
     </>
   );
