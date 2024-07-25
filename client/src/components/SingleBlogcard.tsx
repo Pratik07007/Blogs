38efc7023172e25fcd2bf3c1122b0ce2614d773b
Pratik07007 <s.dhimal006@gmail.com>
2024-07-25 14:20:07 +0545

    single page blog card

--- client/src/components/SingleBlogcard.tsx
+++ client/src/components/SingleBlogcard.tsx
@@ -1,21 +1,32 @@
 import Avatar from "../components/Avatar";
 
 const SinglePageBlogCard = () => {
+  const convertToLocalTime = (dbLocalTime: string) => {
+    var date = new Date(dbLocalTime);
+    return date.toLocaleString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+    });
+  };
+  console.log(convertToLocalTime("2024-07-25T08:09:03.011Z"));
   return (
     <div className="h-screen w-screen ">
       <div className="h-1/2 w-1/2 border border-white">
         <div className="flex items-center gap-4">
-          <Avatar name="Pratik" />
+          <Avatar name="Pratik" size={30} />
           <h1>Pratik Dhimal</h1>
           <h1>Uplaod Time</h1>
         </div>
-        <div><h1>TITLE</h1></div>
         <div>
-            Image
+          <h1>TITLE</h1>
         </div>
-        <div>
-            Desc
-        </div>
+        <div>Image</div>
+        <div>Desc</div>
       </div>
     </div>
   );
--- client/src/pages/Blog.tsx
+++ client/src/pages/Blog.tsx
@@ -1,6 +1,30 @@
+import { useParams } from "react-router-dom";
 import SinglePageBlogCard from "../components/SingleBlogcard";
+import BACKEND_URL from "../utils/Backend_Url";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Blog = () => {
+  const [blog, setBlog] = useState({
+    id: String,
+    title: String,
+    content: String,
+    imageUrl: String,
+    createdAt: String,
+    author: {
+      name: String,
+      id: String,
+    },
+  });
+  const { blogId } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_URL}/blog/single/${blogId}`)
+      .then((res) => setBlog(res.data))
+      .catch((err) => console.error("Error fetching blog:", err));
+  }, [blogId]);
+
   return <SinglePageBlogCard />;
 };
 
