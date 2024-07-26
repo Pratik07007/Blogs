509f9e86ea5c5cf61aeb90dd69c6d71c271a3127
Pratik07007 <s.dhimal006@gmail.com>
2024-07-26 14:23:05 +0545

    nan

--- client/src/components/HomePageBlogs.tsx
+++ client/src/components/HomePageBlogs.tsx
@@ -3,51 +3,58 @@ import HomePageSingleBlog from "./HomePageSingleBlog";
 import axios from "axios";
 import BACKEND_URL from "../utils/Backend_Url";
 import HomePageBlogCardSkleton from "./skeletons/HomePageBlogCardSkleton";
+import Spinner from "./Spinner";
 
 const HomePageBlogs = () => {
   const [blogs, setBlogs] = useState<any[]>([]);
+  const [pageNo, setPageNo] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     axios
-      .get(`${BACKEND_URL}/blog/all`, {
+      .get(`${BACKEND_URL}/blog/all?pageNo=${pageNo}`, {
         headers: {
           Authorization: localStorage.getItem("token"),
         },
       })
       .then((res) => {
-        setBlogs(res.data);
+        setBlogs((prev) => [...prev, ...res.data]);
         setIsLoading(false);
       });
-  }, []);
+  }, [pageNo]);
 
   return (
-    <div className="w-[70vw] flex md:flex-row flex-col items-center  md:justify-center gap-4 flex-wrap  p-4">
+    <div className="w-full flex flex-col">
+      <div className="w-[70vw] flex md:flex-row flex-col items-center  md:justify-center gap-4 flex-wrap  p-4">
+        {isLoading ? (
+          <>
+            <HomePageBlogCardSkleton />
+            <HomePageBlogCardSkleton />
+            <HomePageBlogCardSkleton />
+            <HomePageBlogCardSkleton />
+            <HomePageBlogCardSkleton />
+            <HomePageBlogCardSkleton />
+          </>
+        ) : (
+          blogs.map((blog) => {
+            return (
+              <HomePageSingleBlog
+                key={blog?.id}
+                id={blog?.id}
+                imageUrl={blog?.imageUrl}
+                title={blog?.title}
+                content={blog?.content}
+                author={blog?.author?.name}
+                createdAt={blog?.createdAt}
+              />
+            );
+          })
+        )}
+      </div>
       {isLoading ? (
-        <>
-          <HomePageBlogCardSkleton />
-          <HomePageBlogCardSkleton />
-          <HomePageBlogCardSkleton />
-          <HomePageBlogCardSkleton />
-          <HomePageBlogCardSkleton />
-          <HomePageBlogCardSkleton />
-          <HomePageBlogCardSkleton />
-          <HomePageBlogCardSkleton />
-        </>
-      ) : (
-        blogs.map((blog) => {
-          return (
-            <HomePageSingleBlog
-              key={blog?.id}
-              id={blog?.id}
-              imageUrl={blog?.imageUrl}
-              title={blog?.title}
-              content={blog?.content}
-              author={blog?.author?.name}
-              createdAt={blog?.createdAt}
-            />
-          );
-        })
-      )}
+        <div className="w-full  flex justify-center items-center mt-4">
+          <Spinner />
+        </div>
+      ) : null}
     </div>
   );
 };
